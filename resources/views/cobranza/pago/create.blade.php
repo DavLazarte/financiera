820cb4f792e3e7798881f3f0b685f02d8a74a942
Dav <dav.lazarte@gmail.com>
2018-04-18 17:07:26 -0300

    se agrego mostrar datos del credito antes de cargar el pago

--- resources/views/cobranza/pago/create.blade.php
+++ resources/views/cobranza/pago/create.blade.php
@@ -2,7 +2,7 @@
 @section ('contenido')
 	<div class="row">
 		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-			<h3>Nuevo Pago</h3>
+			<h3>Cargar Pago Nuevo</h3>
 			@if (count($errors)>0)
 			<div class="alert alert-danger">
 				<ul>
@@ -16,27 +16,23 @@
 	</div>
     <div id="msj-success" class="alert alert-success" role="alert" style="display:none">
     <button class="close" data-dismiss="alert"><span>&times;</span></button>
-    Pago Cargado
+        Pago Cargado Correctamente
     </div>
     <div class="row">
     <input type="hidden" name="_token" value="{{ csrf_token() }}" id="token">
         <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
             <div class="form-group">
                 <label for="idventa">Credito</label>
-                <select name="idventa" id="idventa" class="form-control selectpicker" data-live-search="true">
+                <select name="idventa" id="pcredito" class="form-control selectpicker" data-live-search="true">
                     @foreach($creditos as $cred)
-                     <option value="{{$cred->idcredito}}">{{$cred->idcredito.'-'. $cred->nombre_apellido}}</option>
-                     @endforeach
+                     <option value="{{$cred->idcredito}}_{{$cred->zona}}_{{$cred->cliente}}_{{$cred->saldo}}_{{$cred->proyeccion}}_{{$cred->vencimiento}}_{{$cred->estado}}">{{$cred->idcredito.'-'. $cred->nombre_apellido}}</option>
+                    @endforeach
                 </select>
             </div>
-        </div>
-        <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
             <div class="form-group">
                 <label for="fecha">Fecha</label>
                 <input type="date" name="fecha_hora" id="fecha" class="form-control">
             </div>
-        </div>
-        <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
             <div class="form-group">
                 <label for="zona">Zona</label>
                 <select name="zona" class="form-control" id="zona">
@@ -50,16 +46,35 @@
                        <option value="Z7">Z7</option>
                 </select>
             </div>
-        </div>
-        <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
             <div class="form-group">
                 <label for="monto">Monto</label>
                 <input type="text" name="monto" id="monto" value="{{old('monto')}}" class="form-control" placeholder="Monto Abonada">
             </div>
         </div>
-        <input type="hidden" name="estado" value="Activo" id="estado">
 
-    	<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+        <div class="col-lg-3 col-sm-3 col-md-3 col-xs-12">        
+        </div>
+            <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12" id="datoscredito">        
+            <div class="form-group">
+              <label for="estado">Datos del Credito</label>
+              <label for="estado">numero</label>
+              <input type="text" class="form-control"  readonly id="id">
+              <label for="estado">zona</label>                              
+              <input type="text" class="form-control"  readonly id="pzona">                
+              <label for="estado">cliente</label>
+              <input type="text" class="form-control"  readonly id="pcliente">                
+              <label for="estado">saldo</label>
+              <input type="text" class="form-control"  readonly id="saldo">                
+              <label for="estado">proyeccion</label>
+              <input type="text" class="form-control"  readonly id="proyeccion">                
+              <label for="estado">vencimiento</label>
+              <input type="text" class="form-control"  readonly id="vencimiento">                
+              <label for="estado">estado</label>
+              <input type="text" class="form-control"  readonly id="estado">                
+              <input type="hidden" class="form-control" name="estado" value="Activo" id="estado">                
+            </div>
+        </div>
+    	<div class="col-lg-9 col-sm-9 col-md-9 col-xs-12">
     		<div class="form-group">
             	<button class="btn btn-primary" type="submit" id="guardar">Guardar</button>
             	<button class="btn btn-danger" type="reset">Cancelar</button>
@@ -73,8 +88,11 @@ $('#liVentas').addClass("treeview active");
 $('#liClientes').addClass("active");
 </script>
 <script>
+    $("#datoscredito").hide();
+    $("#pcredito").change(mostrarCredito);
+
     $("#guardar").click(function(){
-    var credito    = $("#idventa").val();
+    var credito    = $("#id").val();
     var fecha      = $("#fecha").val();
     var zona       = $("#zona").val();
     var monto      = $("#monto").val();
@@ -103,6 +121,19 @@ $('#liClientes').addClass("active");
         }
     });
 });
+
+function mostrarCredito(){
+    datosCreditos=document.getElementById('pcredito').value.split('_')
+    $("#id").val(datosCreditos[0]);
+    $("#pzona").val(datosCreditos[1]);
+    $("#pcliente").val(datosCreditos[2]);
+    $("#saldo").val(datosCreditos[3]);
+    $("#proyeccion").val(datosCreditos[4]);
+    $("#vencimiento").val(datosCreditos[5]);
+    $("#estado").val(datosCreditos[6]);
+    $("#datoscredito").show();    
+
+}
 </script>
 @endpush
 @endsection
